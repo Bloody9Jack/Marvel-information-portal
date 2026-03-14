a25c222754914b17c3a4190fe0738040bf1cceb1
Кирилл Кравцов <kravtsovkirill@MacBook-Air-Kirill.local>
2026-03-14 17:04:53 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import decoration from '../../resources/img/vision.png';
+
+const App = () => {
+    const [selectedChar, setChar] = useState(null);
+
+    const onCharSelected = (id) => {
+        setChar(id);
+    }
+
+    return (
+        // Оберните всё приложение в <Router>
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <>
+                                <ErrorBoundary>
+                                    <RandomChar />
+                                </ErrorBoundary>
+                                <div className="char__content">
+                                    <ErrorBoundary>
+                                        <CharList onCharSelected={onCharSelected} />
+                                    </ErrorBoundary>
+                                    <ErrorBoundary>
+                                        <CharInfo charId={selectedChar} />
+                                    </ErrorBoundary>
+                                </div>
+                                <img className="bg-decoration" src={decoration} alt="vision" />
+                            </>
+                        </Route>
+                        <Route exact path="/comics">
+                            <>
+                                <AppBanner />
+                                <ComicsList />
+                            </>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
+    );
+}
+
+export default App; 
---
+++ src/services/MarvelService.js
@@ -0,0 +1,76 @@
+import { useHttp } from "../components/hooks/https.hook";
+
+const useMarvelService = () => {
+	const { loading, request, error, clearError } = useHttp();
+
+	const _apiBase = "https://marvel-server-zeta.vercel.app";
+	const _apiKey = "apikey=d4eecb0c66dedbfae4eab45d312fc1df";
+	const _baseOffset = 10;
+
+	const getAllCharacters = async (offset = _baseOffset) => {
+		const res = await request(
+			`${_apiBase}/characters?offset=${offset}&limit=10&${_apiKey}`
+			
+		);
+		return res.map(_transformCharacter);
+	};
+
+	const getCharacter = async (id) => {
+		const res = await request(`${_apiBase}/characters?${id}&${_apiKey}`);
+		return _transformCharacter(res[id]);
+	};
+
+	const getAllComics = async (offset = 0) => {
+		const res = await request(
+			`${_apiBase}/comics?orderBy=issueNumber&limit=9&offset=${offset}&${_apiKey}`
+		);
+		return res.map(_transformComics);
+	};
+
+	const getComics = async (id) => {
+		const res = await request(`${_apiBase}/comics?${id}&${_apiKey}`);
+		return _transformComics(res[id]);
+	};
+
+	const _transformCharacter = (char) => {
+		return {
+			id: char.id,
+			name: char.name,
+			description: char.description
+				? `${char.description.slice(0, 210)}...`
+				: "There is no description for this character",
+			thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			comics: char.comics.items,
+		};
+	};
+
+	const _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+		};
+	};
+
+	return {
+		loading,
+		error,
+		clearError,
+		getAllCharacters,
+		getCharacter,
+		getAllComics,
+		getComics,
+	};
+};
+
+export default useMarvelService;
